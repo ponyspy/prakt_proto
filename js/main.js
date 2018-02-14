860d49fba7e51dad888b9e16f7701c5bc1778ce6
ponyspy <desade4me@gmail.com>
2018-02-14 18:40:19 +0300

    random

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(function() {
+	var randomInteger = function(min, max) {
+		return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+	};
+
 	var swiper = new Swiper('.swiper-container', {
+		initialSlide: randomInteger(0, $('.slide_item').length),
 		pagination: {
 			clickable: true,
 			el: '.swiper-pagination',
