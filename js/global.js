a07730982acab4516235d3d8d42c0f342a7d35fc
ponyspy <desade4me@gmail.com>
2018-02-15 22:42:48 +0300

    menu close

--- js/global.js
+++ js/global.js
@@ -2,4 +2,13 @@ $(function() {
 	$('.menu_open').on('click', function(e) {
 		$('.menu_items, .title_block, .menu_open').toggleClass('open');
 	});
+
+	$(document)
+		.on('mouseup touchend', function(e) {
+			if ($(event.target).closest('.title_block').length) return;
+
+			$('.menu_items, .title_block, .menu_open').removeClass('open');
+
+			event.stopPropagation();
+		});
 });
