925714e32b0961c0092d8af76aa7c88ae03e0579
ponyspy <desade4me@gmail.com>
2018-02-16 17:46:14 +0300

    scrollbars

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 $(function() {
 	if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		$('.timeline_block').on('mousemove', function(e) {
-			var $this = $(this);
+			var $this = $(this).children('.timeline_outer');
 
 			var percent = (e.pageX - $this.offset().left) / $this.width() * 1.1 - 0.25;
 			$this.scrollLeft($this.children('.timeline_inner').width() * percent);
@@ -19,9 +19,11 @@ $(function() {
 
 	$('.month_item')
 		.on('click', function(e) {
+			console.log('month')
 			$('.events_title').attr('data-month', $(this).attr('data-month'));
 			$('.month_item').removeClass('selected').children('.day_item').off('click').end().filter(this).addClass('selected')
 			.children('.day_item').on('click', function(e) {
+				console.log('day')
 				$('.day_item').removeClass('selected').filter(this).addClass('selected');
 			});
 		})
